2af71bc59769ad440f0d0f1bd2b533a971655fd1
ilyas48 <ilyas48@yandex.ru>
2023-07-24 19:33:54 +0300

    new

---
+++ catalog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+    price = models.IntegerField(default=0, verbose_name='цена за покупку')
+    photo = models.ImageField(upload_to='main/', **NULLABLE, verbose_name='изображение(превью)')
+    birth_day = models.DateField(**NULLABLE, verbose_name='дата создания')
+    last_change = models.DateField(**NULLABLE, verbose_name='дата последнего изменения')
+    category = models.CharField(max_length=150, verbose_name='категория')
+
+    def __str__(self):
+        return f'Наименование товара: {self.name},' \
+               f'Категория: {self.category}),' \
+               f'Цена: {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
